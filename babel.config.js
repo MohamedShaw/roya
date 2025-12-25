6c661e8aa1dbaa2092331d9b71346642c5b767ed
MohamedShawky <shawkymohamed138@gmail.com>
2025-12-25 10:58:10 +0200

    install deps

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@hooks': './src/hooks',
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@typings': './src/types',
+          '@utils': './src/utils',
+          '@data': './src/data',
+        },
+      },
+    ],
+    'react-native-worklets/plugin'
+  ],
 };
